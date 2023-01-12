dfc64a2767956eb1788eb9a40d1a194d09fe96fd
SongNoin <as5427072@gmail.com>
2023-01-13 00:55:13 +0900

    FEAT 1. 홈화면 시맨틱 설계 2. 페이지 이동

--- wanted-to-do/src/components/Home/Home.container.tsx
+++ wanted-to-do/src/components/Home/Home.container.tsx
@@ -1,5 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import HomeUI from "./Home.presenter";
 
 export default function HomeContainer() {
-  return <HomeUI />;
+  const navigate = useNavigate();
+
+  function onClickMoveToLogin() {
+    navigate("/login");
+  }
+
+  function onClickMoveToSignUp() {
+    navigate("/signup");
+  }
+
+  return (
+    <HomeUI
+      onClickMoveToLogin={onClickMoveToLogin}
+      onClickMoveToSignUp={onClickMoveToSignUp}
+    />
+  );
 }
--- wanted-to-do/src/components/Home/Home.presenter.tsx
+++ wanted-to-do/src/components/Home/Home.presenter.tsx
@@ -1,3 +1,22 @@
-export default function HomeUI() {
-  return <></>;
+import styled from "styled-components";
+
+interface Props {
+  onClickMoveToLogin: () => void;
+  onClickMoveToSignUp: () => void;
 }
+
+export default function HomeUI({
+  onClickMoveToLogin,
+  onClickMoveToSignUp,
+}: Props) {
+  return (
+    <>
+      <Wrap>
+        <button onClick={onClickMoveToLogin}>로그인</button>
+        <button onClick={onClickMoveToSignUp}>회원가입</button>
+      </Wrap>
+    </>
+  );
+}
+
+const Wrap = styled.div``;
