4c109c469df3c2c1fe0fc15fba8a5d92491af073
SongNoin <as5427072@gmail.com>
2023-01-21 22:55:51 +0900

    FEAT react-query 사용을 위한 app.tsx 환경 설정

--- wanted-to-do/src/App.tsx
+++ wanted-to-do/src/App.tsx
@@ -1,7 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRouter from "./commons/route/AppRouter";
 
 function App() {
-  return <AppRouter />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
